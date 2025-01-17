0bb62e334aa5fe6f70fbdc586d85c3fcb4752a45
jjv432 <jackvranicar43@gmail.com>
2025-01-17 10:57:04 -0500

    Submission version

--- Matlab_II_Ball/Part2.m
+++ Matlab_II_Ball/Part2.m
@@ -4,81 +4,81 @@ clc; clear; close all; format compact
     In order to model the spring-mass damper effect, I am consider a single
     small mass 'suspsended' inside of the casing of the outside of the
     ball.  This way I am able to use a non-zero height for the dynamics of
-    the bounce phase
+    the bounce phase. 
 %}
 ball_response(3);
 
 
 %% Function
 function ball_response(initial_height)
-state = "flight";
+    state = "flight";
 
-ball_radius = .2;
-spring_natrual_length = ball_radius;
+    ball_radius = .2;
+    spring_natrual_length = ball_radius;
 
-figure();
-hold on
-grid on
+    figure();
+    hold on
+    grid on
 
-time = 0:.01:5;
-init = [initial_height 0];
-last_end_time = 0;
+    time = 0:.01:5;
+    init = [initial_height 0];
+    last_end_time = 0;
 
-% This stops the solving when the ball is on the ground
-flight_options = odeset('Events', @flight_event_func);
-stance_options = odeset('Events', @stance_event_func);
+    % This stops the solving when the ball is on the ground
+    flight_options = odeset('Events', @flight_event_func);
+    stance_options = odeset('Events', @stance_event_func);
 
-% Calculate the response for multiple bounces
-for i = 1:9
+    % Calculate the response for multiple bounces
+    for i = 1:9
 
-    switch(state)
+        switch(state)
 
-        case "flight"
-            % Calculate the response
-            [T1, Y1] = ode45(@flight_dynamics, time, init, flight_options);
+            case "flight"
+                % Calculate the response
+                [T1, Y1] = ode45(@flight_dynamics, time, init, flight_options);
 
-            % Plot the response
-            h1 = plot(T1 + last_end_time, Y1(:, 1), 'b--', 'DisplayName', 'Flight');
-            xlabel("Time (s)")
-            ylabel("Ball Height at Center (m)")
-            
-            title("Ball Height vs Time")
+                % Plot the response
+                h1 = plot(T1 + last_end_time, Y1(:, 1), 'b--', 'DisplayName', 'Flight');
+                xlabel("Time (s)")
+                ylabel("Ball Height at Center (m)")
 
-            % Set the new initial value for the next solving.  Algebraically, the
-            % velocity is determined.  Last_end_time is used for plotting the
-            % bounces sequentially
-            init = [Y1(end, 1), Y1(end, 2)];
-            last_end_time = max(T1) + last_end_time;
-            state = "stance";
-            clear T1 Y1
+                title("Ball Height vs Time")
 
-        case "stance"
-             % Calculate the response
-            [T1, Y1] = ode45(@stance_dynamics, time, init, stance_options);
+                % Set the new initial value for the next solving.  Algebraically, the
+                % velocity is determined.  Last_end_time is used for plotting the
+                % bounces sequentially
+                init = [Y1(end, 1), Y1(end, 2)];
+                last_end_time = max(T1) + last_end_time;
+                state = "stance";
+                clear T1 Y1
 
-            % Plot the response
-            h2 = plot(T1 + last_end_time, Y1(:, 1), 'r--', 'DisplayName', 'Stance');
-            xlabel("Time (s)")
-            ylabel("Ball Height at Center (m)")
-            
-            title("Ball Height vs Time")
+            case "stance"
+                % Calculate the response
+                [T1, Y1] = ode45(@stance_dynamics, time, init, stance_options);
 
-            % Set the new initial value for the next solving.  Algebraically, the
-            % velocity is determined.  Last_end_time is used for plotting the
-            % bounces sequentially
-            init = [Y1(end, 1), Y1(end, 2)];
-            last_end_time = max(T1) + last_end_time;
-            state = "flight";
-            clear T1 Y1
+                % Plot the response
+                h2 = plot(T1 + last_end_time, Y1(:, 1), 'r--', 'DisplayName', 'Stance');
+                xlabel("Time (s)")
+                ylabel("Ball Height at Center (m)")
 
+                title("Ball Height vs Time")
+
+                % Set the new initial value for the next solving.  Algebraically, the
+                % velocity is determined.  Last_end_time is used for plotting the
+                % bounces sequentially
+                init = [Y1(end, 1), Y1(end, 2)];
+                last_end_time = max(T1) + last_end_time;
+                state = "flight";
+                clear T1 Y1
+
+
+        end
 
     end
+    legend([h2 h1], "Stance", "Flight");
 
-end
-legend([h2 h1], "Stance", "Flight");
-
-% This function is used to determine when an 'event' occurs; i.e. when
-% the ball hits the ground
+    % This function is used to determine when an 'event' occurs; i.e. when
+    % the ball hits the ground
     function [position,isterminal,direction] = flight_event_func(t,y)
         position = y(1) - ball_radius; % The value that we want to be zero
         isterminal = 1;  % Halt integration
@@ -91,7 +91,7 @@ legend([h2 h1], "Stance", "Flight");
         direction = 1;   % Zero approached by increasing values
     end
 
-% Dynamics of the system using the force of gravity as the only force
+    % Dynamics of the system using the force of gravity as the only force
     function func = flight_dynamics(t, y)
 
         g = -9.81;
@@ -105,6 +105,8 @@ legend([h2 h1], "Stance", "Flight");
         func = [y1p; y2p];
     end
 
+    % Dynamics of the system when it is acting as a mass-spring-damper
+    % during stance
     function func = stance_dynamics(t,y)
 
         g = 9.81;
