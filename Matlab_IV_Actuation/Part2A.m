d2e9ed3fbbea95139c8a354ef5bdeb8047f5b4bd
jjv432 <jackvranicar43@gmail.com>
2025-01-29 22:35:08 -0500

    Ball bounces and returns to same height.  Fixes were made by leaving heavy_stance at a height much smaller than the ball radius.  Then, kp was tuned for the heavy dynamics, and kd made zero

--- Matlab_IV_Actuation/Part2A.m
+++ Matlab_IV_Actuation/Part2A.m
@@ -1,11 +1,7 @@
 clc; clear; close all; format compact
 
 %{
-    This is a simple animation of the ball bouncing using the spring-mass
-    -damper model of ground contact
 
-I think I should add a new state to the system: stance and rest in order to
-determine when to make that instantaneous jump
 %}
 
 
@@ -41,17 +37,20 @@ function draw_ball(Ball, Kinematics, drop_height)
     fps = 60;
     vid.FrameRate = fps;
     vid.Quality = 85;
-    open(vid);
+    % open(vid);
 
-    for i = 1:fps*10 %Ensures video is 10 seconds long
+    for i = 1:numel(Kinematics.Y_Position) %Ensures video is 10 seconds long
         cla
         fill(Ball.X_Points, Ball.Y_Points + Kinematics.Y_Position(i), 'r');
         drawnow;
-        writeVideo(vid, getframe(gcf));
+    
+
+        % writeVideo(vid, getframe(gcf));
 
 
     end
-    close(vid);
+    % close(vid);
+    figure(); plot(Kinematics.Y_Position)
 end
 function Kinematics = ball_response(initial_height, plotBool)
 
@@ -59,16 +58,7 @@ function Kinematics = ball_response(initial_height, plotBool)
 
     state = "flight";
 
-    ball_radius = .02;
-    spring_natrual_length = ball_radius;
-
-    if plotBool
-        figure();
-        hold on
-        grid on
-    end
-
-    time = 0:.01:5;
+    time = [0, 100];
     init = [initial_height 0];
     last_end_time = 0;
 
@@ -79,8 +69,7 @@ function Kinematics = ball_response(initial_height, plotBool)
 
     % Calculate the response for multiple bounces
     maxT = 0;
-    while (maxT < 5)
-        
+    for i = 1:15
 
         switch(state)
 
@@ -88,22 +77,13 @@ function Kinematics = ball_response(initial_height, plotBool)
                 % Calculate the response
                 [T1, Y1] = ode45(@flight_dynamics, time, init, flight_options);
 
-                % Plot the response
-                if plotBool
-                    h1 = plot(T1 + last_end_time, Y1(:, 1), 'b--', 'DisplayName', 'Flight');
-                    xlabel("Time (s)")
-                    ylabel("Ball Height at Center (m)")
-
-                    title("Ball Height vs Time")
-                end
-
                 % Set the new initial value for the next solving.  Algebraically, the
                 % velocity is determined.  Last_end_time is used for plotting the
                 % bounces sequentially
                 init = [Y1(end, 1), Y1(end, 2)];
                 last_end_time = max(T1) + last_end_time;
                 state = "stance_light";
-                Kinematics.Y_Position = [Kinematics.Y_Position; Y1];
+                Kinematics.Y_Position = [Kinematics.Y_Position; Y1(:, 1)];
                 maxT = max(T1);
                 clear T1 Y1
 
@@ -111,21 +91,15 @@ function Kinematics = ball_response(initial_height, plotBool)
                 % Calculate the response
                 [T1, Y1] = ode45(@stance_light_dynamics, time, init, light_stance_options);
 
-                % Plot the response
-                if plotBool
-                    h2 = plot(T1 + last_end_time, Y1(:, 1), 'r--', 'DisplayName', 'Stance');
-                    xlabel("Time (s)")
-                    ylabel("Ball Height at Center (m)")
-
-                    title("Ball Height vs Time")
-                end
                 % Set the new initial value for the next solving.  Algebraically, the
                 % velocity is determined.  Last_end_time is used for plotting the
                 % bounces sequentially
                 init = [Y1(end, 1), Y1(end, 2)];
+                % init = [Y1(end, 1), 5];
+                
                 last_end_time = max(T1) + last_end_time;
                 state = "stance_heavy";
-                Kinematics.Y_Position = [Kinematics.Y_Position; Y1];
+                Kinematics.Y_Position = [Kinematics.Y_Position; Y1(:, 1)];
                 maxT = max(T1);
                 clear T1 Y1
 
@@ -133,21 +107,13 @@ function Kinematics = ball_response(initial_height, plotBool)
                 % Calculate the response
                 [T1, Y1] = ode45(@stance_heavy_dynamics, time, init, heavy_stance_options);
 
-                % Plot the response
-                if plotBool
-                    h3 = plot(T1 + last_end_time, Y1(:, 1), 'r--', 'DisplayName', 'Stance');
-                    xlabel("Time (s)")
-                    ylabel("Ball Height at Center (m)")
-
-                    title("Ball Height vs Time")
-                end
                 % Set the new initial value for the next solving.  Algebraically, the
                 % velocity is determined.  Last_end_time is used for plotting the
                 % bounces sequentially
                 init = [Y1(end, 1), Y1(end, 2)];
                 last_end_time = max(T1) + last_end_time;
                 state = "flight";
-                Kinematics.Y_Position = [Kinematics.Y_Position; Y1];
+                Kinematics.Y_Position = [Kinematics.Y_Position; Y1(:, 1)];
                 maxT = max(T1);
                 clear T1 Y1
 
@@ -155,78 +121,87 @@ function Kinematics = ball_response(initial_height, plotBool)
         end
 
     end
-    if plotBool
-        legend([h2 h1 h3], "Stance", "Flight", "Flight");
-    end
+end
 
-    % This function is used to determine when an 'event' occurs; i.e. when
-    % the ball hits the ground
-    function [position,isterminal,direction] = flight_event_func(t,y)
-        position = y(1) - ball_radius; % The value that we want to be zero
-        isterminal = 1;  % Halt integration
-        direction = -1;   % Zero approached by decreasing values
-    end
+% This function is used to determine when an 'event' occurs; i.e. when
+% the ball hits the ground
+function [position,isterminal,direction] = flight_event_func(t,y)
+    % disp("Flight**********************");
+    % y
+    % keyboard
+    ball_radius = .02;
+    position = y(1) - ball_radius; % The value that we want to be zero
+    isterminal = 1;  % Halt integration
+    direction = -1;   % Zero approached by decreasing values
+end
 
-    function [position,isterminal,direction] = stance_light_event_func(t,y)
-        position = y(2); % The value that we want to be zero
-        isterminal = 1;  % Halt integration
-        direction = -1;   
-    end
-
-    function [position,isterminal,direction] = stance_heavy_event_func(t,y)
-        position = y(1) - ball_radius; % The value that we want to be zero
-        isterminal = 1;  % Halt integration
-        direction = 1;   % Zero approached by increasing values
-    end
-
-    % Dynamics of the system using the force of gravity as the only force
-    function func = flight_dynamics(t, y)
-
-        g = -9.81;
-
-        y1 = y(1);
-        y2 = y(2);
-
-        y1p = y(2);
-        y2p = g;
-
-        func = [y1p; y2p];
-    end
-
-    % Dynamics of the system when it is acting as a mass-spring-damper
-    % during stance
-    function func = stance_light_dynamics(t,y)
-
-        g = 9.81;
-        kp = 500;
-        kd = 5;
-        m = .5;
-        L = spring_natrual_length;
-
-        y1 = y(1);
-        y2 = y(2);
-
-        y1p = y(2);
-        y2p = kp*(L - y(1))/m - kd*y1p/m -g;
-
-        func = [y1p; y2p];
-    end
-
-    function func = stance_heavy_dynamics(t,y)
-
-        g = 9.81;
-        kp = 10000;
-        kd = 5;
-        m = .5;
-        L = spring_natrual_length;
-
-        y1 = y(1);
-        y2 = y(2);
-
-        y1p = y(2);
-        y2p = kp*(L - y(1))/m - kd*y1p/m -g;
-
-        func = [y1p; y2p];
-    end
+function [position,isterminal,direction] = stance_light_event_func(t,y)
+    
+    position = y(2); % The value that we want to be zero
+    isterminal = 1;  % Halt integration
+    direction = 0;
 
 end
+
+function [position,isterminal,direction] = stance_heavy_event_func(t,y)
+    % disp("Heavy**********************");
+    % y
+    % keyboard
+    ball_radius = .02;
+    position = y(1) -.0005; % The value that we want to be zero
+    isterminal = 1;  % Halt integration
+    direction = 1;   % Zero approached by increasing values
+end
+
+% Dynamics of the system using the force of gravity as the only force
+function func = flight_dynamics(t, y)
+
+    g = -9.81;
+
+    y1 = y(1);
+    y2 = y(2);
+
+    y1p = y(2);
+    y2p = g;
+
+    func = [y1p; y2p];
+end
+
+% Dynamics of the system when it is acting as a mass-spring-damper
+% during stance
+function func = stance_light_dynamics(t,y)
+
+    g = 9.81;
+    kp = 250;
+    kd = 7;
+    m = .5;
+    spring_natural_length =.02;
+    L = spring_natural_length;
+
+    y1 = y(1);
+    y2 = y(2);
+
+    y1p = y(2);
+    y2p = kp*(L - y(1))/m - kd*y1p/m -g;
+
+    func = [y1p; y2p];
+end
+
+function func = stance_heavy_dynamics(t,y)
+
+    g = 9.81;
+    kp = 580;
+    kd = 0;
+    m = .5;
+    spring_natural_length = .02;
+    L = spring_natural_length;
+
+    y1 = y(1);
+    y2 = y(2);
+
+    y1p = y(2);
+    y2p = kp*(L - y(1))/m - kd*y1p/m -g;
+
+    func = [y1p; y2p];
+end
+
