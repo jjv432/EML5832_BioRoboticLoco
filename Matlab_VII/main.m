0c5c4042bdacd37fc9ef6bb55363b652dcfc904a
jjv432 <jackvranicar43@gmail.com>
2025-03-24 20:25:26 -0400

    Relatively close. Going for two phases of each state. X does not seem to behave correctly. Got here by adding the last X value from flight to the X values from stance and by setting init for stance to the actual values

--- Matlab_VII/main.m
+++ Matlab_VII/main.m
@@ -8,7 +8,7 @@ params.b = 0;
 params.k = 500;
 params.g = 9.81;
 params.m = 10;
-params.t_hip = -100;
+params.t_hip = 1000;
 % params.t_hip = 50;
 % params.t_hip = 0;
 time = [0 30];
@@ -29,7 +29,7 @@ last_end_time = 0;
 T = [];
 state = 'stance';
 
-for i = 1:2
+for i = 1:4
 
     switch state
 
@@ -38,15 +38,19 @@ for i = 1:2
             [T1, Y1] = ode45(@(T1, Y1) stance_dynamics(T1, Y1, params), time, init, stance_options);
 
             Y1(:, 3) = wrapTo2Pi(Y1(:,3));
-            Y1(:, 3) = -sign(Y1(:,3));
-            x = Y1(end, 1) * -sign(Y1(end, 3))*sin(Y1(end, 3));
+            % Y1(:, 3) = -sign(Y1(:,3));
+            if ~isempty(Kinematics.X)
+                x = Y1(end, 1) * -sign(Y1(end, 3))*sin(Y1(end, 3)) + Kinematics.X(end);
+            else
+                x = Y1(end, 1) * -sign(Y1(end, 3))*sin(Y1(end, 3));
+            end
             % x = Y1(end, 1) * sign(Y1(end, 3))*sin(Y1(end, 3));
             z = Y1(end, 1) * cos(Y1(end, 3));
 
             x_d = Y1(end, 2) * sin(Y1(end, 3)) + Y1(end, 4) * sin(Y1(end, 3)) * Y1(end, 1);
             z_d = Y1(end, 2) * cos(Y1(end, 3)) + Y1(end, 4) * cos(Y1(end, 3)) * Y1(end, 1);
 
-            init = [x; x_d; z; z_d; Y1(end, 3); Y1(end, 4)];
+            init = [x; -x_d; z; z_d; Y1(end, 3); Y1(end, 4)];
 
             T = [T; T1(1:end-1) + last_end_time];
             last_end_time = max(T1) + last_end_time;
@@ -62,11 +66,11 @@ for i = 1:2
             clear T1 Y1
 
         case 'flight'
-            
+
             disp("flight");
             [T1, Y1] = ode45(@(T1, Y1) flight_dynamics(T1, Y1, params), time, init, flight_options);
             % init = [Y1(end, 1), Y1(end, 2)];
-            init = [1; 0; pi/4; 0];
+            init = [1; 0; Y1(end, 5); Y1(end, 6)];
             T = [T; T1(1:end-1) + last_end_time];
             last_end_time = max(T1) + last_end_time;
 
@@ -82,17 +86,17 @@ end
 
 animateHopper(Kinematics);
 
-% figure()
-% axis equal
-% plot(Kinematics.X,Kinematics.Z);
-% title("Z v X")
-% 
-% figure()
-% plot(T, Kinematics.Z);
-% title("Z");
-% figure()
-% plot(T, Kinematics.X);
-% title("X");
+figure()
+axis equal
+plot(Kinematics.X,Kinematics.Z);
+title("Z v X")
+
+figure()
+plot(T, Kinematics.Z);
+title("Z");
+figure()
+plot(T, Kinematics.X);
+title("X");
 
 %% Animation
 
@@ -106,7 +110,7 @@ function animateHopper(Kinematics)
     plot(0, 0, 'x', 'LineWidth', 3);
     for i = 1:length(X)
         h1 = plot(X(i), Z(i), 'rx', 'LineWidth',5);
-        axis([-4 4 -4 4])
+        axis([-4 9 -4 9])
         axis equal
         pause(.1);
 
