072022b07c00d8362526bc0522e0159dd15ccc54
jjv432 <jackvranicar43@gmail.com>
2025-03-27 15:23:24 -0400

    Params function created

--- Matlab_7_SLIP/main.m
+++ Matlab_7_SLIP/main.m
@@ -13,17 +13,9 @@ addpath("src");
 % Show Animation?
 animate = 1;
 
-% Parameters
-params.l0 = 1;  %* m
-params.b = 2;  %*
-params.k = 100; %*
-params.g = 9.81;
-params.m = 1;  %*
-params.t_hip = .276;  %*
-params.phi_0 = -pi/6;  %*
-params.phi_d_0 = 1.5;
-params.l_d_0 = -3.5;
 
+
+params = getParams();
 % Time Vector for ODE45
 time = 0:.1:30;
 
