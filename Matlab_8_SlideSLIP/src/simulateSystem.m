d1ec4acfb2876febc21f8893e62ccdc4dad23dda
jjv432 <jackvranicar43@gmail.com>
2025-03-30 16:13:03 -0400

    Animation is close, likely an initial condition issue

--- Matlab_8_SlideSLIP/src/simulateSystem.m
+++ Matlab_8_SlideSLIP/src/simulateSystem.m
@@ -24,7 +24,7 @@ function [Kinematics, T, nr_results] = simulateSystem(params, time, nr_bool, ini
     if nr_bool
         duration = 2;
     else
-        duration = 2;
+        duration = 5;
     end
 
     for i = 1:duration
@@ -60,9 +60,9 @@ function [Kinematics, T, nr_results] = simulateSystem(params, time, nr_bool, ini
                 x_d_vals = l_vals.*phi_d_vals.*sin(phi_vals) + l_d_vals.*sin(phi_vals);
                 z_d_vals = l_vals.*phi_d_vals.*cos(phi_vals) + l_d_vals.*cos(phi_vals);
 
-                if i>1
-                    x_vals = x_vals - x_vals(1);
-                end
+                % if i>1
+                %     x_vals = x_vals - x_vals(1);
+                % end
 
                 % Store the T, x, and z positions for plotting
                 Kinematics.X = [Kinematics.X; x_vals(1:end-1) + x_offset];
@@ -159,13 +159,13 @@ function [Kinematics, T, nr_results] = simulateSystem(params, time, nr_bool, ini
                 x_d_vals = l_vals.*phi_d_vals.*sin(phi_vals) + l_d_vals.*sin(phi_vals);
                 z_d_vals = l_vals.*phi_d_vals.*cos(phi_vals) + l_d_vals.*cos(phi_vals);
 
-                if i>2
-                    x_vals = x_vals - x_vals(1);
-                    % x_vals_sliding = x_vals_sliding - x_vals_sliding(1);
-                end
+                % if i>2
+                %     x_vals = x_vals - x_vals(1);
+                %     % x_vals_sliding = x_vals_sliding - x_vals_sliding(1);
+                % end
 
                 % Store the T, x, and z positions for plotting
-                Kinematics.X = [Kinematics.X; x_vals(1:end-1) + x_offset];
+                Kinematics.X = [Kinematics.X; x_vals(1:end-1) + x_offset + x_vals_sliding(1:end-1)- x_vals_sliding(1)];
                 Kinematics.X_Slide = [Kinematics.X_Slide; x_vals_sliding(1:end-1)- x_vals_sliding(1)];
                 Kinematics.Z = [Kinematics.Z; z_vals(1:end-1)];
                 Kinematics.Phi = [Kinematics.Phi; phi_vals(1:end-1)];
@@ -173,7 +173,7 @@ function [Kinematics, T, nr_results] = simulateSystem(params, time, nr_bool, ini
                 T = [T; T1(1:end-1) + t_offset, zeros(numel(T1)-1, 1), ones(numel(T1)-1, 1)];
 
                 % Set 'state' and the init vector for the next state
-                init = [x_vals(end) + x_offset; x_d_vals(end); z_vals(end); z_d_vals(end)];
+                init = [x_vals(end) + x_offset + x_vals_sliding(end); x_d_vals(end) + x_d_vals_sliding(end); z_vals(end); z_d_vals(end)];
                 myState = 'flight';
 
         end
