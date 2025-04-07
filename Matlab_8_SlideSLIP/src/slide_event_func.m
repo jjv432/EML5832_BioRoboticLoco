f157752e835f191c6e61a9289d98fd306ea4fb6b
jjv432 <jackvranicar43@gmail.com>
2025-04-06 23:02:13 -0400

    Simplified sliding and stance event function with trig functions

--- Matlab_8_SlideSLIP/src/slide_event_func.m
+++ Matlab_8_SlideSLIP/src/slide_event_func.m
@@ -25,20 +25,21 @@ function [position,isterminal,direction] = slide_event_func(t,y, params)
     direction(1) = -1;
 
     % Transition to stance
-    Fleg = k*(l0 - l) - b*l_d;
-
+    % Fleg = k*(l0 - l) - b*l_d;
+    % 
     persistent muK
     if isempty(muK)
         muK = params.muK;
     end
+    % 
+    % Fleg_x = abs(Fleg*sin(phi));
+    % Fleg_y = abs(Fleg*cos(phi));
+    % Ffk = Fleg_y * muK;
 
-    Fleg_x = abs(Fleg*sin(phi));
-    Fleg_y = abs(Fleg*cos(phi));
-    Ffk = Fleg_y * muK;
-
-    position(2) = Ffk - Fleg_x;
+    % position(2) = Ffk - Fleg_x;
+    position(2) = tan(phi) - muK;
     isterminal(2) = 1;
-    direction(2) = 1;
+    direction(2) = -1;
 
 end
 
