85125336e4f9a5961c517106652dd628c54e1cd1
jjv432 <jackvranicar43@gmail.com>
2025-04-09 23:11:39 -0400

    Shows it working for less damping

--- Matlab_8_SlideSLIP/src/getParams.m
+++ Matlab_8_SlideSLIP/src/getParams.m
@@ -2,7 +2,7 @@ function params = getParams()
 
     % Parameters
     params.l0 = 1;  %* m
-    params.b = 20;  %*
+    params.b = 15;  %*
     params.k = 105; %*
     params.g = 9.81;
     params.m = 1;  %*
