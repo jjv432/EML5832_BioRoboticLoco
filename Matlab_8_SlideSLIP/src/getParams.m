11db019bb9e242c28f4c6595ee569f3cbcb5caf5
jjv432 <jackvranicar43@gmail.com>
2025-04-11 14:53:40 -0400

    Immediately goes into flight

--- Matlab_8_SlideSLIP/src/getParams.m
+++ Matlab_8_SlideSLIP/src/getParams.m
@@ -2,15 +2,15 @@ function params = getParams()
 
     % Parameters
     params.l0 = 1;  %* m
-    params.b = 15;  %*
+    params.b = 10;  %*
     params.k = 105; %*
     params.g = 9.81;
-    params.m = 1;  %*
+    params.m = 1.5;  %*
     params.t_hip = 2.9;  %*
     params.phi_0 = -pi/5.5;  %*
     params.phi_d_0 = 1.5;
     params.l_d_0 = -4;
-    params.muS = .3; % Good for sliding
+    params.muS = .6; % Good for sliding
     params.muK = (.5)*params.muS;
 
 end
