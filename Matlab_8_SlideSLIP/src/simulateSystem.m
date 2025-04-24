fc6f674dc4b84315af706d955eee2ae2e8ac676f
jjv432 <jackvranicar43@gmail.com>
2025-04-24 19:15:14 -0400

    More reasonable, seems to get stuck at length

--- Matlab_8_SlideSLIP/src/simulateSystem.m
+++ Matlab_8_SlideSLIP/src/simulateSystem.m
@@ -147,7 +147,7 @@ function [Kinematics, T, nr_results] = simulateSystem(params, time, nr_bool, ini
                 phi_d_init = l0*(-vz*sin(params.phi_0) + vx*cos(params.phi_0));
 
 
-                if abs(tan(params.phi_0)) > params.muS
+                if abs(tan(params.phi_0)) < params.muS
                     init = [l0; l_d_init; params.phi_0; phi_d_init; x_vals(end) + l0*sin(params.phi_0); vx];
                     state  = 'sliding';
                 else
